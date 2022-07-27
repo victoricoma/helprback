77eb318942de4925a244ab56a66170722befd868
Gustavo Gama <gamatraduz@gmail.com>
2022-07-27 10:15:41 -0300

    Inicialização do projeto Java Spring com MySQL

---
+++ helpback/src/main/java/com/api/helpback/HelpbackApplication.java
@@ -0,0 +1,13 @@
+package com.api.helpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpbackApplication.class, args);
+	}
+
+}
---
+++ helpback/src/test/java/com/api/helpback/HelpbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.helpback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
