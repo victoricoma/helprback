25f734d75ca030000e035b3b0873c10ef5d0e8f6
mcruillas <marinacruillas@gmail.com>
2022-07-27 10:20:39 -0300

    Helper Back

---
+++ hekprback/src/main/java/com/api/hekprback/HekprbackApplication.java
@@ -0,0 +1,13 @@
+package com.api.hekprback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HekprbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HekprbackApplication.class, args);
+	}
+
+}
