7397cc8b22cce7edbbf9b989ba4531ba58a7585e
lucaswotta <lucaswotta@gmail.com>
2022-07-30 22:25:55 -0300

    Gatilhos e Logs com ajuste de Views

--- helprback/mysql/empregados/Script de VIEW com CASE e exercicio.sql
+++ helprback/mysql/empregados/Script de VIEW com CASE e exercicio.sql
@@ -1,11 +1,11 @@
 use empregados_db;
 
 CREATE OR REPLACE VIEW vw_imposto_renda
-AS
+AS 
 SELECT e.nome AS empregado,
 COUNT(d.cod_dep) AS n_dependente,
 (e.salario * 13) AS renda,
-(e.salario * 13) *(COUNT(d.cod_dep) * 0.2) AS abatimento 
+(e.salario * 13) *(COUNT(d.cod_dep) * 0.2) as abatimento
 FROM empregado e
 LEFT JOIN dependente d
 ON e.cod_empregado = d.cod_empregado
@@ -21,24 +21,22 @@ ROLLBACK;
 COMMIT;
 
 START TRANSACTION;
-UPDATE empregado SET dt_nascimento = "1982-10-19", sexo = "F" WHERE cod_empregado=2;
+UPDATE empregado SET dt_nascimento = "1982-10-19", sexo='F' WHERE cod_empregado=2;
 ROLLBACK;
 COMMIT;
 
-SELECT *, IF(renda>50000, "IRPF Alto", "IRPF Normal") AS faixa FROM vw_imposto_renda;
+SELECT *, IF(renda>50000,"IRPF alto","IRPF Normal") as faixa FROM vw_imposto_renda;
 
 SELECT empregado,
-CASE 
-	WHEN renda < 22848 THEN "Faixa 1 [0]"	
+CASE
+	WHEN renda < 22848 THEN "Faixa1 [0]"
     WHEN renda < 33920 THEN "Faixa 2 [7,5]"
-    WHEN renda < 45013 THEN "Faixa 3 [15]"
+    WHEN renda < 45013 THEN "Faixa 3 [1]"
     WHEN renda < 55977 THEN "Faixa 4 [22,5]"
-    ELSE "Faixa 5 [27,5]"
+    ELSE "Faixa 5 [27,5]" 
 END AS faixa,
-(renda - abatimento) AS renda_real
+(renda - abatimento) as renda_real
 FROM vw_imposto_renda;
 
 START TRANSACTION;
 UPDATE empregado SET salario = 8200.30 WHERE cod_empregado=2;
-ROLLBACK;
-COMMIT;
