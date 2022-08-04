58a47b5912eb1d317d24180defb0651d3dbe5db9
Gustavo Gama <gamatraduz@gmail.com>
2022-08-04 09:54:28 -0300

    Adição das classes enumeradas do sistema.

--- helpback/src/main/java/com/api/helpback/HelpbackApplication.java
+++ helpback/src/main/java/com/api/helpback/HelpbackApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelpbackApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelpbackApplication.class, args);
+		System.out.println("API do web App Helper rodando!");
 	}
 
 }
