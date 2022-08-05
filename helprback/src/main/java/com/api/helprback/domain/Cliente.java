f5941bc125415dec68553420b49ac1c13d39959f
William-Sanches <eng.williamsg@outlook.com>
2022-08-05 11:39:10 -0300

    Estruturação das classes para persistência de Entidades.

--- helprback/src/main/java/com/api/helprback/domain/Cliente.java
+++ helprback/src/main/java/com/api/helprback/domain/Cliente.java
@@ -1,9 +1,17 @@
 package com.api.helprback.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Pessoa {
+
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamado = new ArrayList<>();
 
     public Cliente(){
