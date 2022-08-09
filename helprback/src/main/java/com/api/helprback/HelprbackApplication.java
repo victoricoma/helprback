0384dec85422c9832a3f1dce037b83a164a9c3d0
Warley <warley@protonmail.com>
2022-08-09 09:49:06 -0300

    Injeção de dados

--- helprback/src/main/java/com/api/helprback/HelprbackApplication.java
+++ helprback/src/main/java/com/api/helprback/HelprbackApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelprbackApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelprbackApplication.class, args);
-		System.out.println("API do Web App Helpr rodando!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelprbackApplication.class, args);
+        System.out.println("API do Web App Helpr rodando!");
+    }
 
 }
+
+
