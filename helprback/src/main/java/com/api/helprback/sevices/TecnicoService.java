3fa515c46645f58ada343ea49548d8d1d159c85a
Warley <warley@protonmail.com>
2022-08-10 11:02:35 -0300

    Adição da listagem de tecnico por serviço e resourde por Id.

---
+++ helprback/src/main/java/com/api/helprback/sevices/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.api.helprback.sevices;
+
+import com.api.helprback.domain.Tecnico;
+import com.api.helprback.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
