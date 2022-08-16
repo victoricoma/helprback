9720030358e0054571ac318bff012da7fd73ac7f
talitabarros13 <talita.barros1@hotmail.com>
2022-08-16 16:01:47 -0300

    Criação de metodo CRUD para classe clientes.

--- helprback/src/main/java/com/api/helprback/domain/Cliente.java
+++ helprback/src/main/java/com/api/helprback/domain/Cliente.java
@@ -1,9 +1,13 @@
 package com.api.helprback.domain;
 
+import com.api.helprback.domain.dtos.ClienteDTO;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 public class Cliente extends Pessoa{
 
@@ -17,6 +21,16 @@ public class Cliente extends Pessoa{
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+    }
+    public Cliente(ClienteDTO objDto){
+        super();
+        this.id = objDto.getId();
+        this.nome = objDto.getNome();
+        this.cpf = objDto.getCpf();
+        this.email = objDto.getEmail();
+        this.senha = objDto.getSenha();
+        this.perfis = objDto.getPerfis().stream().map(x ->x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = objDto.getDataCriacao();
     }
 
     public List<Chamado> getChamados() {
