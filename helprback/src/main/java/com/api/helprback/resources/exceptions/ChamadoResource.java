104aa14000684bc662d72675c427829a6d85ba2d
aurireads <comealongmari@gmail.com>
2022-08-17 12:01:44 -0300

    Serviço de relatório do Chamado por Id com relacionamento.

---
+++ helprback/src/main/java/com/api/helprback/resources/exceptions/ChamadoResource.java
@@ -0,0 +1,24 @@
+package com.api.helprback.resources.exceptions;
+
+import com.api.helprback.domain.Chamado;
+import com.api.helprback.domain.dtos.ChamadoDTO;
+import com.api.helprback.services.exceptions.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/service/chamados")
+public class ChamadoResource {
+    @Autowired
+    private ChamadoService chamadoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+        Chamado obj = this.chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
