e54c2d232c1cb984e28654b5773dbdde076c65da
Warley <warley@protonmail.com>
2022-08-17 16:13:47 -0300

    Criação Métodos Chamado

---
+++ helprback/src/main/java/com/api/helprback/services/ChamadoService.java
@@ -0,0 +1,29 @@
+package com.api.helprback.services;
+
+import com.api.helprback.domain.Chamado;
+import com.api.helprback.repositories.ChamadoRepository;
+import com.api.helprback.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findBy(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Não existe chamado com ID: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
+
+}
+
+
+
