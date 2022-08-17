f2de21a3311dde37c6f102c49e8f51a915a77de2
AdrianaNeres <adrcaneres@gmail.com>
2022-08-17 13:54:55 -0300

    Listagem geral de Chamados para tabela.

--- helprback/src/main/java/com/api/helprback/resources/ChamadoResource.java
+++ helprback/src/main/java/com/api/helprback/resources/ChamadoResource.java
@@ -1,4 +1,5 @@
 package com.api.helprback.resources;
+
 import com.api.helprback.domain.Chamado;
 import com.api.helprback.domain.dtos.ChamadoDTO;
 import com.api.helprback.services.ChamadoService;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/service/chamados")
@@ -20,5 +24,11 @@ public class ChamadoResource {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         Chamado obj = this.chamadoService.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDto = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
--- helprback/src/main/java/com/api/helprback/services/ChamadoService.java
+++ helprback/src/main/java/com/api/helprback/services/ChamadoService.java
@@ -1,20 +1,26 @@
 package com.api.helprback.services;
 
 import com.api.helprback.domain.Chamado;
+import com.api.helprback.domain.dtos.ChamadoDTO;
 import com.api.helprback.repositories.ChamadoRepository;
 import com.api.helprback.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
+    private Function<? super ChamadoDTO,?> obj;
 
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("O chamado não existe em nosso sistema."));
     }
+
+    public List<Chamado> findAll() {return repository.findAll();}
 }
